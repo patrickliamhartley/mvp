868e8f4fc6cd25d4e570cc1e820ae006a8ab9c32
Patrick Hartley <patrickliamhartley@Patricks-MacBook-Pro.local>
2016-08-29 15:31:35 -0700

    Implement killing of enemies with gold accumulation

--- client/app/battlefield/battlefield.js
+++ client/app/battlefield/battlefield.js
@@ -1,9 +1,21 @@
 angular.module('app.battlefield',[])
-  .controller('BattlefieldController', ['$scope', '$rootScope', function ($scope, $rootScope) {
-    
+  .controller('BattlefieldController', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {
+    $scope.gold=0;
+
     $scope.showName = function() {
       $scope.name = $rootScope.name;
     //   console.log($rootScope.name);
+    };
+
+    $scope.fade= function() {
+      $timeout(function (){
+        $scope.hidden=true;
+      },2000);
+    };
+
+    $scope.kill= function (){
+      $scope.hidden=true;
+      $scope.gold++;
     };
     
   }]);  
