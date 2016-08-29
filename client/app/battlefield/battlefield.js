b914de9c6e8c537e168899ae3bc4a0f5f6267f04
Patrick Hartley <patrickliamhartley@Patricks-MacBook-Pro.local>
2016-08-29 16:08:29 -0700

    Implement random gen for enemy stats

--- client/app/battlefield/battlefield.js
+++ client/app/battlefield/battlefield.js
@@ -1,22 +1,32 @@
-angular.module('app.battlefield',[])
+angular.module('app.battlefield', [])
   .controller('BattlefieldController', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {
-    $scope.gold=0;
-    $scope.enemies= $rootScope.enemies;
+    $scope.gold = 0;
+    $scope.enemies = $rootScope.enemies;
+
+    $scope.place = function () {
+      for (var i = 0; i < $rootScope.difficulty; i ++) {
+        $scope.enemies[i].x = Math.random() * $rootScope.width;
+        $scope.enemies[i].y = Math.random() * $rootScope.height;
+        $scope.enemies[i].time = Math.random() * 5;
+      }
+
+    };
 
     $scope.showName = function() {
       $scope.name = $rootScope.name;
     //   console.log($rootScope.name);
     };
 
-    $scope.fade= function() {
-      $timeout(function (){
-        $scope.hidden=true;
-      },2000);
+    $scope.fade = function() {
+      $timeout(function () {
+        $scope.hidden = true;
+      }, 5000);
     };
 
-    $scope.kill= function (){
-      $scope.hidden=true;
+    $scope.kill = function () {
+      $scope.hidden = true;
       $scope.gold++;
+      console.log($scope.enemies);
     };
     
   }]);  
--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,10 +1,14 @@
 angular.module('app.home',[])
   .controller('HomeController', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
     
+
+
     $scope.add = function(val) {
       $rootScope.name = val;
       $rootScope.enemies = [];
       $rootScope.difficulty=$scope.difficulty;
+      $rootScope.height = window.innerHeight; 
+      $rootScope.width = window.innerWidth;
       for (var i = 0; i <$scope.difficulty; i ++){
         $rootScope.enemies.push({
           x: null,
